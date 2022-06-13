fd91a7da10b9e6d2b41f25fe8192519cb7840ce0
blackbird <98256610+blackbird173@users.noreply.github.com>
2022-06-13 12:12:06 -0400

    wheel_of_food.rs

--- wheel_of_food.rs
+++ wheel_of_food.rs
@@ -1,6 +1,8 @@
+// This program allows the user to play a game and win prizes by guessing the correct word.
 use std::{io, io::Write, fs::OpenOptions};
 #[derive(Debug)]
 enum Prizes {
+   // This enums stores all the possible prizes.
    Plushie,
    RubberDucky,
    Fan,
@@ -11,38 +13,48 @@ enum Prizes {
 }
 fn main() {
    let words = vec!["cheese", "sauce", "pasta", "grapes", "orange", "salad", "taco"];
+   // These are the possible words that can be chosen.
    println!("spinning...");
    let mut word = String::from(words[&0 as *const i32 as usize % 7 as usize]);
    let mut blank = vec!['-'; word.len()];
-   println!("the word has {} chars and every wrong guess makes ur money go down by 10 dollars (the word is a food and it starts with a {})", word.len(), word.chars().collect::<Vec<char>>()[0]);
-   let backup = word.clone();
-   let mut money = 10000000;
+   println!("the word has {} chars and every wrong guess makes ur money go down by 10 dollars (the word is a food and it starts with a {})", word.len(), &word[0..1]);
+   let backup = word.clone(); // This variable stores a copy of the word for the game.
+   let mut money = 10000000; // This is the starting money.
    loop {
       println!("money: {} and word: {}", money, blank.iter().collect::<String>());
       if backup == blank.iter().collect::<String>() {
-         if money <= 100 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::Plushie, money);
-         } else if money <= 9999949 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::RubberDucky, money);
-         } else if money <= 9999959 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::Fan, money);
-         } else if money <= 9999969 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::BathTub, money);
-         } else if money <= 9999979 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::InDoorTentBed, money);
-         } else if money <= 9999989 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::Boat, money);
-         } else if money >= 9999999 {
-            println!("ur prize is a {:?} and {} dollars", Prizes::Car, money);
+         match money { //This matches all the possible rewards to the money.
+            0..=9999948 => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::Plushie, money);
+            },
+            9999949..=9999958  => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::RubberDucky, money);
+            },
+            9999959..=9999968  => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::Fan, money);
+            },
+            9999969..=9999978  => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::BathTub, money);
+            },
+            9999979..=9999988  => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::InDoorTentBed, money);
+            },
+            9999989..=9999998  => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::Boat, money);
+            },
+            9999999..=10000000 => {
+               println!("ur prize is a {:?} and {} dollars", Prizes::Car, money);
+            },
+            _ => {},
          }
-         let end = "u wont ".to_owned() + money.to_string().as_str() + " and a prize\n";
-         let mut file = OpenOptions::new().create(true).append(true).open("wheel_of_food.txt").unwrap();
+         let end = "u won ".to_owned() + money.to_string().as_str() + " and a prize\n";
+         let mut file = OpenOptions::new().create(true).append(true).open("wheel_of_food.txt").unwrap(); // This creates a new file for appending your winning stats.
          file.write_all(end.as_bytes()).unwrap();
          break;
       }
-      let mut guess = String::new();
+      let mut guess = String::new(); // This creates a guess for what the letters could be.
       io::stdin().read_line(&mut guess).unwrap();
-      let _ = guess.trim().chars().map(|x| {
+      let _ = guess.trim().chars().map(|x| { // This iters through guess and matches letters to the word and reveals them.
          if word.find(x) != None {
             if word.contains(x) {
                blank[word.find(x).unwrap()] = x;
@@ -51,7 +63,7 @@ fn main() {
                word = vec_word.iter().collect::<String>();
             }
          }
-         if !backup.contains(x) {
+         if !backup.contains(x) { // If it is a wrong letter it subtracts money.
             money -= 10;
          }
          x
