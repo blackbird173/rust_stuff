8c4e8c395ec95691168ceb1e3a8bcb60e82d7602
blackbird <98256610+blackbird173@users.noreply.github.com>
2022-06-13 12:33:17 -0400

    drawing_board.rs

--- drawing_board.rs
+++ drawing_board.rs
@@ -1,17 +1,20 @@
+// This program allows the user to move around and place/erase spots.
 use std::io;
 fn main() {
     let mut z = true;
-    let mut grid = String::from("[---------------------------------------------------------------------------------------]\n");
+    let mut grid = String::from("[---------------------------------------------------------------------------------------]\n"); // Creates the grid.
     for _ in 0..27 {
-        grid.push_str("[.......................................................................................]\n");
+        grid.push_str("[.......................................................................................]\n"); // Appends to the grid.
     }
-    grid.push_str("[---------------------------------------------------------------------------------------]");
+    grid.push_str("[---------------------------------------------------------------------------------------]"); // Appends to the grid.
+    // The bounds of the grid are the dashes and the [].
     let mut pos = 112;
-    let mut g = grid.chars().collect::<Vec<char>>();
-    let mut inp = String::new();
+    let mut g = grid.chars().collect::<Vec<char>>(); // Converts the grid into a vector so we can change chars.
     loop {
-        io::stdin().read_line(&mut inp).unwrap();
+      let mut inp = String::new(); // Variable for user input.
+        io::stdin().read_line(&mut inp).unwrap(); // Reads user input.
         match inp.as_str().trim() {
+           // These match statements are used to tell the guy to move up, down, left, or right, they can also be used to place spaces or erase spaces.
             "w" => {
                 if g[pos-90] == '.' || g[pos-90] == '*' {
                     pos -= 90;
@@ -151,9 +154,6 @@ fn main() {
             _ => {
             },
         }
-        println!("{}", g.iter().collect::<String>());
-        if inp != String::new() {
-            inp = String::new();
-        }
+        println!("{}", g.iter().collect::<String>()); // Prints out g as a string.
     }
 }
